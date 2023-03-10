ed77a044130f75a61072b481f963299c37ff0a58
sher1999 <sersodishankulov521@gmail.com>
2023-03-10 15:56:25 +0500

    index  example  design

--- Api/Models/ViewModels/CreateOrderVm.cs
+++ Api/Models/ViewModels/CreateOrderVm.cs
@@ -6,24 +6,24 @@ namespace Api.Models.ViewModels;
 public class CreateOrderVm
 {
     [Required]
-    [Display(Name = "Рақами ҳуҷҷат")]
+    [Display(Name = "Р/т")]
     public string DocumentNumber { get; set; } = null!;
     [Required]
     [Display(Name = "Санаи воридшавии ҳуҷҷат")]
     public string ImportDate { get; set; } = null!;
     [Required]
-    [Display(Name = "Номи ҳуҷҷат")]
+    [Display(Name = "№ ва шохиси ҳуҷҷатҳои воридшуда")]
     public string DecreeName { get; set; } = null!;
     [Required]
-    [Display(Name = "Мавзуъ")]
+    [Display(Name = "Ирсолкунанда")]
     public string Title { get; set; } = null!;
     [Required]
     [Display(Name = "Ҳуҷҷатро интихоб кунед")]
     public IFormFile DecreeFile { get; set; } = null!;
     [DataType(DataType.DateTime)]
-    [Display(Name = "Муҳлоти иҷроиш")]
+    [Display(Name = "Муҳлати иҷрои ҳуҷҷат")]
     public DateTime Deadline { get; set; }
-    [Display(Name = "Иҷрокунандаҳо")]
+    [Display(Name = "Амри хаттӣ ё ба кӣ фиристода шудани ҳуҷҷат")]
     public List<SelectListItem> Users { get; set; } = new();
     public List<int> UserIds { get; set; } = new(); 
 }
--- Api/Models/ViewModels/EditOrderVm.cs
+++ Api/Models/ViewModels/EditOrderVm.cs
@@ -7,16 +7,18 @@ public class EditOrderVm
 {
     public int Id { get; set; }
     [Required]
-    [Display(Name = "Мавзуъ")]
+    [Display(Name = "Ирсолкунанда")]
     public string Title { get; set; } = null!;
     public int StatusId { get; set; }
+    [Display(Name = "Ҷавоби ҳуҷҷат")]
     public string? ExecutionDocumentName { get; set; }
     public IFormFile? ResponseFile { get; set; }
     [Required]
-    [Display(Name = "Муҳлоти иҷроиш")]
+    [Display(Name = "Муҳлати иҷрои ҳуҷҷат")]
     public DateTime Deadline { get; set; }
     public List<SelectListItem> Users { get; set; } = new();
     public List<int> UserIds { get; set; } = new();
     public string? ExecutionFilePath { get; set; }
+    [Display(Name = "Тамдиди ҳуҷҷат")]
     public DateTime? ExtraDeadline { get; set; }
 }
