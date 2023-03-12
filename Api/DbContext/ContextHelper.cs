cccaf33bd0138b88d3791e0830d87f146c04ae9d
AzizBobokhujaev <aziz.bobohojaev@gmail.com>
2023-03-12 19:10:05 +0500

    Добавил доступы

--- Api/DbContext/ContextHelper.cs
+++ Api/DbContext/ContextHelper.cs
@@ -25,6 +25,15 @@ public static class ContextHelper
             };
             await roleManager.CreateAsync(userRole);
         }
+        if (!roleManager.Roles.Where(p=>p.NormalizedName.Equals("SuperAdmin")).Any())
+        {
+            var userRole = new IdentityRole<int>
+            {
+                Name = "SuperAdmin",
+                NormalizedName = "SUPERADMIN"
+            };
+            await roleManager.CreateAsync(userRole);
+        }
         if (!userManager.Users.Where(p=>p.UserName.Equals("Admin")).Any())
         {
             var adminUser = new User
@@ -39,6 +48,22 @@ public static class ContextHelper
                 var role = await roleManager.FindByNameAsync("Admin");
 
                 await userManager.AddToRoleAsync(await userManager.FindByNameAsync("Admin"), role.Name);
+            }
+        }
+        if (!userManager.Users.Where(p=>p.UserName.Equals("SuperAdmin")).Any())
+        {
+            var adminUser = new User
+            {
+                UserName = "SuperAdmin",
+                Email = "SuperAdmin@mail.ru"
+            };
+            var result = await userManager.CreateAsync(adminUser, "Super_@dm!n_1999");
+
+            if (result.Succeeded)
+            {
+                var role = await roleManager.FindByNameAsync("SuperAdmin");
+
+                await userManager.AddToRoleAsync(await userManager.FindByNameAsync("SuperAdmin"), role.Name);
             }
         }
     }
